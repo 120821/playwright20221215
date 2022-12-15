1ac85908171ddfac63b428ad9a029589c9a911ce
linlin <1208215066@qq.com>
2022-12-15 10:56:38 +0800

    增加了button的测试

--- tests/locator.spec.js
+++ tests/locator.spec.js
@@ -1,22 +1,28 @@
-// @ts-check
 const { test, expect } = require('@playwright/test');
 
 test('localhost locator', async ({ page }) => {
   await page.goto('http://localhost:4567/');
 
   // Expect a title "to contain" a substring.
-//  await expect(page).toHaveTitle(/localhost/);
+  // await expect(page).toHaveTitle(/localhost/);
   await page.getByRole('button', { name: 'Sign in' }).click();
 
+  // 每次将定位器用于操作时，都会在页面中找到一个最新的 DOM 元素。
+  // 在下面的代码片段中，底层 DOM 元素将被定位两次，一次在每个动作之前。
+  // 这意味着如果 DOM 由于重新渲染而在调用之间发生变化，则将使用与定位器对应的新元素。
+  const locator = page.getByRole('button', { name: 'Sign in' })
+
+  await locator.hover();
+  await locator.click();
   // create a locator
-//  const getStarted = page.getByRole('link', { name: 'Get started' });
+  // const getStarted = page.getByRole('link', { name: 'Get started' });
 
   // Expect an attribute "to be strictly equal" to the value.
-//  await expect(getStarted).toHaveAttribute('href', '/docs/intro');
+  // await expect(getStarted).toHaveAttribute('href', '/docs/intro');
 
   // Click the get started link.
-//  await getStarted.click();
+  // await getStarted.click();
 
   // Expects the URL to contain intro.
-//  await expect(page).toHaveURL(/.*intro/);
+  // await expect(page).toHaveURL(/.*intro/);
 });
