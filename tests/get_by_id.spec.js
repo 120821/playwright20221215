86f37b2db6dc64f684825affb4bbf3f987f56bde
linlin <1208215066@qq.com>
2022-12-16 11:53:13 +0800

    增加了等待的时间

--- tests/get_by_id.spec.js
+++ tests/get_by_id.spec.js
@@ -2,7 +2,10 @@ const { test, expect } = require('@playwright/test');
 
 test('localhost locator', async ({ page }) => {
   await page.goto('http://localhost:4567/get_by_id');
+  // wait for 1 second
+  await page.waitForTimeout(5000);
   await page.getByTestId('orange').click();
+  await page.waitForTimeout(3000);
   //const banana = await page.getByRole('listitem').nth(1);
 
 });
