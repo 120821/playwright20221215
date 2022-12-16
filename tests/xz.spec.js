04629bffa79e08eea9a85bf20f14c632a0111623
linlin <1208215066@qq.com>
2022-12-16 15:53:26 +0800

    精简了对阿里云博客的抓取

--- tests/xz.spec.js
+++ tests/xz.spec.js
@@ -5,31 +5,16 @@ test('xz aliyun pages', async({ page })=>{
   await page.goto("https://xz.aliyun.com/tab/4")
   // title of existing page
   console.log(await page.title());
-  console.log(await page.url());
-
-  await page.waitForTimeout(5000);
-  await page.locator('text=下一页').click()
-  console.log(await page.title());
-  console.log(await page.url());
-
-  await page.waitForTimeout(5000);
-  await page.locator('text=下一页').click()
-  console.log(await page.title());
-  console.log(await page.url());
-
-  await page.waitForTimeout(5000);
-  await page.locator('text=下一页').click()
-  console.log(await page.title());
-  console.log(await page.url());
-
-  await page.waitForTimeout(5000);
-  await page.locator('text=下一页').click()
-  console.log(await page.title());
-  console.log(await page.url());
-
-  await page.waitForTimeout(5000);
-  await page.locator('text=下一页').click()
-  console.log(await page.title());
   console.log(await page.content());
   console.log(await page.url());
+
+  for(var i = 0; i<10; i++){
+    console.log(i)
+    await page.waitForTimeout(5000);
+    await page.locator('text=下一页').click()
+    //console.log(await page.content());
+    console.log(await page.title());
+    console.log(await page.url());
+  }
+
 })
