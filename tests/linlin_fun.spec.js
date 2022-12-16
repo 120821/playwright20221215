2abb9113a1b7e4e22b8929499ce991f27195ea38
linlin <1208215066@qq.com>
2022-12-16 16:55:29 +0800

    增加了network和琳琳博客的抓取脚本，都没有好

---
+++ tests/linlin_fun.spec.js
@@ -0,0 +1,32 @@
+const { test, expect } = require('@playwright/test');
+
+test.only('localhost locator', async ({ context}) => {
+  const page = await context.newPage();
+  await page.goto("https://www.linlin.fun/blogs")
+  const html = await page.content();
+  console.log(await page.title());
+  console.log(await page.url());
+  console.log("====== html")
+  console.log(html);
+
+  for(var i = 0; i<5; i++){
+    console.log(i)
+    await page.waitForTimeout(5000);
+    page.locator('text=加载更多').first().click()
+    console.log(await page.title());
+    console.log(await page.url());
+    // 打印的数据不是需要的div数据
+    // 是这样的
+    // Promise { <pending> }
+    // 先注释了
+    //const div = page.locator('div .tab-content').textContent();
+    //console.log('=== div content')
+    //console.log(div)
+    //
+    //console.log(await page.content());
+    await page.waitForTimeout(5000);
+  }
+
+
+});
+
--- tests/network.spec.js
+++ tests/network.spec.js
@@ -1,14 +1,44 @@
-// @ts-check
+//// @ts-check
 
+//(async () => {
+//  const browser = await playwright.webkit.launch();  // firefox Or 'chromium' or 'webkit'.
+//  // Create a new incognito browser context.
+//  const context = await browser.newContext();
+//  // Create a new page in a pristine context.
+//  const page = await context.newPage();
+//  await page.goto('https://example.com');
+
+//  // Gracefully close up everything
+//  await context.close();
+//  await browser.close();
+//})();
+const playwright = require('@playwright/test');
+const launchOptions = {
+  proxy: {
+    server: 'https://doveip.com/index.php?s=/index/login.html&lang=en-us',
+    //server: 'localhost:1090',
+    username: 'bigbanana666',
+    password: 'bigbanana888'
+  }
+};
 (async () => {
-  const browser = await playwright.webkit.launch();  // firefox Or 'chromium' or 'webkit'.
-  // Create a new incognito browser context.
-  const context = await browser.newContext();
-  // Create a new page in a pristine context.
+  const defaultBrowser = await playwright['chromium'].launch({});
+  const context = await defaultBrowser.newContext();
   const page = await context.newPage();
-  await page.goto('https://example.com');
+  await page.goto('https://froxy.com/api/detect-ip');
+  console.log('chromium without proxy:', await page.textContent("*"));
+  await defaultBrowser.close();
 
-  // Gracefully close up everything
-  await context.close();
-  await browser.close();
+  for (const browserType of ['chromium', 'firefox', 'webkit']) {
+    const browser = await playwright[browserType].launch(launchOptions);
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    try {
+      await page.goto('https://froxy.com/api/detect-ip');
+      console.log(`${browserType} with proxy:`, await page.textContent("*"));
+    } catch (e) {
+      console.log(e);
+    }
+    await browser.close();
+  }
 })();
