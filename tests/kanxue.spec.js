c72b3fbbaebaa6bb86b58cc891cdb3aa46f06da3
linlin <1208215066@qq.com>
2022-12-16 15:22:01 +0800

    打印看雪前三页的div【class=‘tab-content‘】的内容，尚未完善

--- tests/kanxue.spec.js
+++ tests/kanxue.spec.js
@@ -13,9 +13,13 @@ test.only('localhost locator', async ({ context}) => {
   console.log(await page.title());
   console.log(await page.url());
   page.locator('text=加载更多').first().click()
+  console.log('=== div content')
+  const div = page.locator('div .tab-content').textContent();
+  //await page.locator('article:has(div.promo)').textContent();
   console.log(await page.title());
   console.log(await page.url());
   console.log("====== html")
+  console.log(div)
   //console.log(await page.content());
   await page.waitForTimeout(5000);
 });
